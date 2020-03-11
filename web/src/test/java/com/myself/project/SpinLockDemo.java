5c3242392d9f16a80b9be2c9f16dba14435dc56b
zzbb <1870287706@qq.com>
2020-03-12 00:15:20 +0800

    自旋锁代码

---
+++ web/src/test/java/com/myself/project/SpinLockDemo.java
@@ -0,0 +1,59 @@
+package com.myself.project;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SpinLockDemo {
+
+    AtomicReference<Thread> atomicReference = new AtomicReference();
+
+    public void myLock(){
+        Thread thread = Thread.currentThread();
+        System.out.println("thread = " + Thread.currentThread().getName() + "  come in");
+        while (!atomicReference.compareAndSet(null,thread)){
+            System.out.println("TimeUnit = " + System.currentTimeMillis());
+        }
+
+    }
+
+    public void myUnLock(){
+
+        Thread thread = Thread.currentThread();
+        atomicReference.compareAndSet(thread,null);
+        System.out.println(Thread.currentThread().getName() + " invoke myUnLock");
+    }
+
+    public static void main(String[] args) {
+        SpinLockDemo lockDemo = new SpinLockDemo();
+
+        new Thread(()->{
+            lockDemo.myLock();
+
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            lockDemo.myUnLock();
+        },"AAA").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(()->{
+            lockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            lockDemo.myUnLock();
+        },"BBB").start();
+
+
+
+    }
+}
--- web/src/test/java/com/myself/project/WebApplicationTests.java
+++ web/src/test/java/com/myself/project/WebApplicationTests.java
@@ -1,5 +1,6 @@
 package com.myself.project;
 
+import lombok.Data;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,46 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WebApplicationTests {
 
+	@Data
+	class Phone{
+
+		private String id;
+
+		private String name;
+
+		private String idCard;
+
+		private int number;
+
+		public void  addNumber(){
+			number++;
+		}
+
+	}
+
 	@Test
 	public void contextLoads() {
+
+		Phone phone = new Phone();
+
+		new Thread(()->{
+			phone.addNumber();
+			System.out.println("phone = " + phone.getNumber());
+
+		},"线程1").start();
+
+		new Thread(()->{
+			phone.addNumber();
+			System.out.println("phone = " + phone.getNumber());
+
+		},"线程2").start();
+
+
+		new Thread(()->{
+			phone.addNumber();
+			System.out.println("phone = " + phone.getNumber());
+		},"线程3").start();
+
 	}
 
 }
